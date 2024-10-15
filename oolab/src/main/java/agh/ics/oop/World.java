b504429963703c943a4bda6fc591c4d1cafad1a7
Blajszu <oskar.blajsz@gmail.com>
2024-10-16 01:22:06 +0200

    modify run method to accept args only in MoveDirection type

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,19 +1,21 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
     public static void main(String[] args) {
-        System.out.println("System wystartowal!");
-        run(args);
+        System.out.println("System wystarowal!");
+        run(OptionsParser.parse(args));
         System.out.println("System zakonczyl dzialanie!");
     }
 
-    public static void run(String[] args) {
-        for (String arg : args) {
-            switch (arg) {
-                case "f" -> System.out.println("Zwierzak idzie do przodu!");
-                case "b" -> System.out.println("Zwierzak idzie do tylu!");
-                case "l" -> System.out.println("Zwierzak skreca w lewo!");
-                case "r" -> System.out.println("Zwierzak skreca w prawo!");
+    public static void run(MoveDirection[] directions) {
+        for (MoveDirection direction : directions) {
+            switch (direction) {
+                case MoveDirection.FORWARD -> System.out.println("Zwierzak idzie do gory!");
+                case MoveDirection.BACKWARD -> System.out.println("Zwierzak idzie w dol!");
+                case MoveDirection.RIGHT -> System.out.println("Zwierzak skreca w prawo!");
+                case MoveDirection.LEFT -> System.out.println("Zwierzak skreca w lewo!");
             }
         }
     }
