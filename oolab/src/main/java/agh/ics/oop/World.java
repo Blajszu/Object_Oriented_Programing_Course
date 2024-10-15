a194b857736faf3d9fefc49d0e6d619dbe98d830
Blajszu <oskar.blajsz@gmail.com>
2024-10-16 01:13:18 +0200

    modify run method to print information about direction

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -6,16 +6,15 @@ public class World {
         run(args);
         System.out.println("System zakonczyl dzialanie!");
     }
-    public static void run(String[] args) {
-        System.out.println("Zwierzak idzie do przodu!");
-        if (args.length == 0) {
-            return;
-        }
 
-        String result = args[0];
-        for (int i = 1; i < args.length; i++) {
-            result += ", " + args[i];
+    public static void run(String[] args) {
+        for (String arg : args) {
+            switch (arg) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu!");
+                case "b" -> System.out.println("Zwierzak idzie do tylu!");
+                case "l" -> System.out.println("Zwierzak skreca w lewo!");
+                case "r" -> System.out.println("Zwierzak skreca w prawo!");
+            }
         }
-        System.out.println(result);
     }
 }
