669f2713dfb5b43d4589baa2882e8c99e81ed9b9
Blajszu <oskar.blajsz@gmail.com>
2024-10-16 01:02:12 +0200

    modify run method to print arguments

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,19 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartowal!");
-        run();
+        run(args);
         System.out.println("System zakonczyl dzialanie!");
     }
-    public static void run() {
+    public static void run(String[] args) {
         System.out.println("Zwierzak idzie do przodu!");
+        if (args.length == 0) {
+            return;
+        }
+
+        String result = args[0];
+        for (int i = 1; i < args.length; i++) {
+            result += ", " + args[i];
+        }
+        System.out.println(result);
     }
 }
