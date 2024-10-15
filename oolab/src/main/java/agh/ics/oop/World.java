ddc6e6e355b9cc5d10e56b1a6809a0c2387667ae
Blajszu <oskar.blajsz@gmail.com>
2024-10-16 00:53:54 +0200

    add main method to World class

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,6 +2,7 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-
+        System.out.println("System wystartowal!");
+        System.out.println("System zakonczyl dzialanie!");
     }
 }
