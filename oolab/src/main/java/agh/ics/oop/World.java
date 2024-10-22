e340c77d6efe462d37b3b5a666a36ae5a7dd1a33
Blajszu <oskar.blajsz@gmail.com>
2024-10-22 12:55:30 +0200

    delete tests from World.java

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,19 +1,12 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystarowal!");
         run(OptionsParser.parse(args));
         System.out.println("System zakonczyl dzialanie!");
-
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
     }
 
     private static void run(MoveDirection[] directions) {
