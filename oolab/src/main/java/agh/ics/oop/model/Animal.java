b61695197410e71234865a3eaff7ab91853d8ebe
Blajszu <oskar.blajsz@gmail.com>
2024-10-29 21:55:21 +0100

    Add method move to Animal Class

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,6 +1,9 @@
 package agh.ics.oop.model;
 
 public class Animal {
+    private static final Vector2d LEFT_DOWN_MAP_CORNER = new Vector2d(0,0);
+    private static final Vector2d RIGHT_UP_MAP_CORNER = new Vector2d(4,4);
+    
     private MapDirection currentOrientation;
     private Vector2d currentPositionOnMap;
 
@@ -30,4 +33,30 @@ public class Animal {
         return currentPositionOnMap.equals(position);
     }
 
+    public void move(MoveDirection direction) {
+        if(direction == null)
+            return;
+
+        Vector2d newPosition = currentPositionOnMap;
+
+        switch (direction) {
+            case MoveDirection.LEFT:
+                currentOrientation = currentOrientation.previous();
+                break;
+            case MoveDirection.RIGHT:
+                currentOrientation = currentOrientation.next();
+                break;
+            case MoveDirection.FORWARD:
+                newPosition = currentPositionOnMap.add(currentOrientation.toUnitVector());
+                break;
+            case MoveDirection.BACKWARD:
+                newPosition = currentPositionOnMap.subtract(currentOrientation.toUnitVector());
+                break;
+        }
+
+        if(
+            (newPosition.getX() >= LEFT_DOWN_MAP_CORNER.getX() && newPosition.getY() >= LEFT_DOWN_MAP_CORNER.getY()) &&
+            (newPosition.getX() <= RIGHT_UP_MAP_CORNER.getX() && newPosition.getY() <= RIGHT_UP_MAP_CORNER.getY())
+        ) currentPositionOnMap = newPosition;
+    }
 }
