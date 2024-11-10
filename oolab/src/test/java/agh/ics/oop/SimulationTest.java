fb1b0ee46b1b9c9be840629dd3a08ba16b4ac3f0
Blajszu <oskar.blajsz@gmail.com>
2024-11-10 16:10:38 +0100

    add test for orientation to simulation tests

--- oolab/src/test/java/agh/ics/oop/SimulationTest.java
+++ oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -2,6 +2,7 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.MapDirection;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -25,13 +26,19 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
 
         // Then
         assertEquals(2, animalPositions.size());
+        assertEquals(2, animalOrientations.size());
+
         for (Vector2d pos : animalPositions) {
             assertTrue(pos.follows(new Vector2d(0, 0)));
             assertTrue(pos.precedes(new Vector2d(4, 4)));
         }
+
+        assertEquals(MapDirection.EAST, animalOrientations.get(0));
+        assertEquals(MapDirection.EAST, animalOrientations.get(1));
     }
 
     @Test
@@ -47,12 +54,13 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
         Vector2d position = animalPositions.getFirst();
-        Vector2d expectedPosition = new Vector2d(0, 1);
 
         // Then
         assertEquals(1, animalPositions.size());
-        assertEquals(expectedPosition, position);
+        assertEquals(new Vector2d(0, 1), position);
+        assertEquals(MapDirection.EAST, animalOrientations.getFirst());
     }
 
     @Test
@@ -70,11 +78,14 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
 
         // Then
         assertEquals(2, animalPositions.size());
         assertEquals(new Vector2d(4, 4), animalPositions.get(0));
         assertEquals(new Vector2d(0, 0), animalPositions.get(1));
+        assertEquals(MapDirection.EAST, animalOrientations.get(0));
+        assertEquals(MapDirection.SOUTH, animalOrientations.get(1));
     }
 
     @Test
@@ -94,12 +105,17 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
 
         // Then
         assertEquals(3, animalPositions.size());
         assertEquals(new Vector2d(1, 2), animalPositions.get(0));
         assertEquals(new Vector2d(4, 2), animalPositions.get(1));
         assertEquals(new Vector2d(3, 4), animalPositions.get(2));
+
+        assertEquals(MapDirection.NORTH, animalOrientations.get(0));
+        assertEquals(MapDirection.EAST, animalOrientations.get(1));
+        assertEquals(MapDirection.NORTH, animalOrientations.get(2));
     }
 
     @Test
@@ -112,10 +128,12 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
 
         // Then
         assertEquals(1, animalPositions.size());
         assertEquals(new Vector2d(2, 2), animalPositions.getFirst());
+        assertEquals(MapDirection.NORTH, animalOrientations.getFirst());
     }
 
     @Test
@@ -131,8 +149,10 @@ public class SimulationTest {
         // When
         simulation.run();
         List<Vector2d> animalPositions = simulation.getAnimalsPositionsAfterSimulation();
+        List<MapDirection> animalOrientations = simulation.getAnimalOrientationsAfterSimulation();
 
         // Then
         assertEquals(0, animalPositions.size());
+        assertEquals(0, animalOrientations.size());
     }
 }
