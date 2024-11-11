8187d0c18ea20773163fa2fd47e4c88f2732c1cf
Blajszu <oskar.blajsz@gmail.com>
2024-11-11 16:02:31 +0100

    add additional simulation for Strings in main method in World class

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,16 +1,26 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.*;
-
 import java.util.List;
 
 public class World {
     public static void main(String[] args) {
 
-        WorldMap wm = new RectangularMap(5, 5);
+        WorldMap<Animal, Vector2d> wm = new RectangularMap(5, 5);
         List<MoveDirection> directions = OptionsParser.parse(args);
-        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-        Simulation simulation = new Simulation(positions, directions, wm);
+
+        List<Animal> animals = List.of(new Animal(new Vector2d(2,2)), new Animal(new Vector2d(3,4)));
+        Simulation<Animal, Vector2d> simulation = new Simulation<>(animals, directions, wm);
+
         simulation.run();
+
+        //Dodatkowa symulacja dla napisów
+
+        List<String> list = List.of("Ala", "ma", "kota", "i", "psa");
+
+        WorldMap<String, Integer> map = new TextMap();
+        Simulation<String, Integer> simulation2 = new Simulation<>(list, directions, map);
+
+        simulation2.run();
     }
 }
