42d0d05df979604eaec4f72a98bf1f296fc7c21f
Blajszu <oskar.blajsz@gmail.com>
2024-11-19 20:51:24 +0100

    add IllegalArgumentException to OptionsParser

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,15 +2,21 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class World {
     public static void main(String[] args) {
 
-        WorldMap wm = new GrassField(10);
-        List<MoveDirection> directions = OptionsParser.parse(args);
-        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-        Simulation simulation = new Simulation(positions, directions, wm);
-        simulation.run();
+        try {
+            WorldMap wm = new GrassField(10);
+            List<MoveDirection> directions = OptionsParser.parse(args);
+            List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+            Simulation simulation = new Simulation(positions, directions, wm);
+            simulation.run();
+        }
+        catch (IllegalArgumentException e) {
+            e.printStackTrace(System.err);
+        }
     }
 }
