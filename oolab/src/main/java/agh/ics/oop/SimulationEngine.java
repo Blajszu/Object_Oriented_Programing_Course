d5a3cdbea17f59e287c3cb54764ebe3db73d1dbc
Blajszu <oskar.blajsz@gmail.com>
2024-11-28 14:14:30 +0100

    add async running simulations

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -1,16 +1,35 @@
 package agh.ics.oop;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SimulationEngine {
 
     private final List<Simulation> simulations;
+    private final List<Thread> simulationThreads = new ArrayList<>();
 
     public SimulationEngine(List<Simulation> simulations) {
+
         this.simulations = simulations;
+        simulations.forEach(simulation -> {
+            simulationThreads.add(new Thread(simulation));
+        });
     }
 
     public void runSync() {
         simulations.forEach(Simulation::run);
+    }
+
+    public void runAsync() {
+        simulationThreads.forEach(Thread::start);
+    }
+
+    public void awaitSimulationsEnd() throws InterruptedException {
+        for (Thread thread : simulationThreads) {
+            thread.join();
+        }
     }
 }
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,29 +3,43 @@ package agh.ics.oop;
 import agh.ics.oop.model.*;
 import agh.ics.oop.model.util.ConsoleMapDisplay;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class World {
     public static void main(String[] args) {
 
         try {
-            GrassField gf = new GrassField(10);
-            RectangularMap rm = new RectangularMap(10, 10);
-
-            gf.addObserver(new ConsoleMapDisplay());
-            rm.addObserver(new ConsoleMapDisplay());
-
             List<MoveDirection> directions = OptionsParser.parse(args);
             List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+            List<Simulation> simulations = new ArrayList<>();
+            ConsoleMapDisplay display = new ConsoleMapDisplay();
 
-            Simulation simulation1 = new Simulation(positions, directions, gf);
-            Simulation simulation2 = new Simulation(positions, directions, rm);
+            for(int i = 0; i < 500; i++) {
+                GrassField gf = new GrassField(10);
+                RectangularMap rm = new RectangularMap(10, 10);
 
-            SimulationEngine engine = new SimulationEngine(List.of(simulation1, simulation2));
-            engine.runSync();
+                gf.addObserver(display);
+                rm.addObserver(display);
+
+                Simulation simulation1 = new Simulation(positions, directions, gf);
+                Simulation simulation2 = new Simulation(positions, directions, rm);
+
+                simulations.add(simulation1);
+                simulations.add(simulation2);
+            }
+
+            SimulationEngine engine = new SimulationEngine(simulations);
+            engine.runAsync();
+            engine.awaitSimulationsEnd();
         }
         catch (IllegalArgumentException e) {
             e.printStackTrace(System.err);
         }
+        catch (InterruptedException e) {
+            System.out.printf("Action interrupted: %s%n", e.getMessage());
+        }
+
+        System.out.println("System zakonczyl dzialanie!");
     }
 }
