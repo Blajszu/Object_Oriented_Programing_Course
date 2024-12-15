7e080e153d8678b79b95bbb99af406fd0134a6c0
Blajszu <oskar.blajsz@gmail.com>
2024-12-15 21:59:24 +0100

    add working graphical simulation animation

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -4,29 +4,87 @@ import agh.ics.oop.OptionsParser;
 import agh.ics.oop.Simulation;
 import agh.ics.oop.SimulationEngine;
 import agh.ics.oop.model.*;
+import agh.ics.oop.model.util.Boundary;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.util.List;
 
 public class SimulationPresenter implements MapChangeListener {
-    private WorldMap worldMap;
 
-    @FXML
-    private Label infoLabel;
+    private final int CELL_WIDTH = 38;
+    private final int CELL_HEIGHT = 38;
+
+    private WorldMap worldMap;
+    private Boundary currentBounds;
+
     @FXML
     private Label moveLabel;
     @FXML
     private TextField moveList;
+    @FXML
+    public GridPane mapGrid;
+
+    private void clearGrid() {
+        mapGrid.getChildren().retainAll(mapGrid.getChildren().getFirst()); // hack to retain visible grid lines
+        mapGrid.getColumnConstraints().clear();
+        mapGrid.getRowConstraints().clear();
+    }
+
+    private void addLabels(int mapHeight, int mapWidth) {
+        Label label = new Label("x/y");
+        GridPane.setHalignment(label, HPos.CENTER);
+        mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+        mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+        mapGrid.add(label, 0, 0);
+
+        for(int i = 0; i <= mapHeight; i++) {
+            Label labelY = new Label(String.valueOf(currentBounds.upperRight().getY() - i));
+            GridPane.setHalignment(labelY, HPos.CENTER);
+            mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+            mapGrid.add(labelY, 0, i+1);
+        }
+
+        for(int i = 0; i <= mapWidth; i++) {
+            Label labelX = new Label(String.valueOf( currentBounds.lowerLeft().getX() + i));
+            GridPane.setHalignment(labelX, HPos.CENTER);
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+            mapGrid.add(labelX, i+1, 0);
+        }
+    }
+
+    private void addWorldElements() {
+        List<WorldElement> worldElements = (List<WorldElement>) worldMap.getElements();
+
+        worldElements.forEach(worldElement -> {
+            Label worldElementLabel = new Label(worldElement.toString());
+            GridPane.setHalignment(worldElementLabel, HPos.CENTER);
+            Vector2d position = worldElement.getPosition();
+            mapGrid.add(worldElementLabel,
+                    position.getX() + 1 - currentBounds.lowerLeft().getX(),
+                    currentBounds.upperRight().getY() - position.getY() + 1);
+        });
+    }
 
     public void setWorldMap(WorldMap worldMap) {
         this.worldMap = worldMap;
     }
 
     public void drawMap() {
-        infoLabel.setText(worldMap.toString());
+        clearGrid();
+
+        currentBounds = worldMap.getCurrentBounds();
+        int mapHeight = currentBounds.upperRight().getY() - currentBounds.lowerLeft().getY();
+        int mapWidth = currentBounds.upperRight().getX() - currentBounds.lowerLeft().getX();
+
+        addLabels(mapHeight, mapWidth);
+        addWorldElements();
     }
 
     @Override
@@ -39,6 +97,8 @@ public class SimulationPresenter implements MapChangeListener {
     }
 
     public void onSimulationStartClicked() {
+        mapGrid.setGridLinesVisible(true);
+
         try {
             GrassField map = new GrassField(10);
             this.setWorldMap(map);
