c4b49080098d8c8b28395a393710d89ae30cd741
Blajszu <oskar.blajsz@gmail.com>
2024-12-15 21:18:54 +0100

    add WorldGUI and SimulationApp Classes

---
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -0,0 +1,18 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.RectangularMap;
+import agh.ics.oop.presenter.SimulationPresenter;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SimulationApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
